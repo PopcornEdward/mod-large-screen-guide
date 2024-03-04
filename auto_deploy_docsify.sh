427750b2b96a5a00364640583c0788cfe41f5957
fangh <marklow@126.com>
2024-03-04 11:10:01 +0800

    D777

--- auto_deploy_docsify.sh
+++ auto_deploy_docsify.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+chmod 777 website
 # 删除之前部署的网站对应的目录
 rm -rf ./website
 # 克隆仓库, 将会把项目所有内容克隆到当前目录的repo目录下(如果repo目录不存在会创建)
 git clone ssh://root@192.168.1.109:2222/frontend/doc-screen-front-api.git website
+chmod 777 website
 # 将Dockerfile克隆到website目录的docs目录下 第一次测试
 cp ./Dockerfile ./website/docs
 # 构建docker镜像
